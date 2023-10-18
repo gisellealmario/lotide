43401a1a7f2d4eaf2908584825dfec6913c8c3f7
gisellealmario <gisellealmario@gmail.com>
2023-10-18 12:04:28 +0000

    eslint

--- head.js
+++ head.js
@@ -1,18 +1,18 @@
 // Define the head function
-function head(array) {
+const head = function(array) {
   if (array.length === 0) {
     return undefined;
   }
   return array[0];
-}
+};
 
 // Define the assertEqual function
-function assertEqual(actual, expected) {
+const assertEqual = function(actual, expected) {
   console.assert(actual === expected,`${actual} !== ${expected}`);
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   }
-}
+};
 
 // Test cases
 assertEqual(head([5, 6, 7]), 5); // Should pass
