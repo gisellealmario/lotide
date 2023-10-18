a673c313046a36a21c0f89e7eda1d5897ba05ebc
gisellealmario <gisellealmario@gmail.com>
2023-10-18 10:50:43 +0000

    assert

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const errorMsg = "👎👎👎";
+const passed = "👍👍👍";
+
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${errorMsg} Assertion failed: Expected ${expected}, but got ${actual}`);
+  if (actual === expected) {
+    console.log(`${passed} Assertion passed: Expected ${expected} and got ${actual}`);
+  }
+};
+
+
+// Test the function
+assertEqual(1, 1);
