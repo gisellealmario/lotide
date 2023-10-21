da90a70771f9d8a944398c1f887add02cd3db766
gisellealmario <gisellealmario@gmail.com>
2023-10-21 21:40:57 +0000

    complete

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// Example usage:
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // Fail
