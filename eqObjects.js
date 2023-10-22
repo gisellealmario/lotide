471a58972ebe419555bfa8c69441f44ab46dc56c
gisellealmario <gisellealmario@gmail.com>
2023-10-22 21:18:24 +0000

    eqobjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,25 @@
-// This function checks if the actual and expected values are equal and logs the appropriate message
+// Function to check if two arrays are equal
+const eqArrays = function(actual, expected) {
+  // Check if the lengths of the arrays are equal
+  if (actual.length !== expected.length) {
+    return false; // If the lengths are different, the arrays are not equal
+  }
+  // Iterate through the elements of the arrays
+  for (let i = 0; i < actual.length; i++) {
+    // Check if each element in the arrays is the same
+    if (actual[i] !== expected[i]) {
+      return false; // If any elements are different, the arrays are not equal
+    }
+  }
+  return true; // If no differences are found, the arrays are equal
+};
+
+// Function to log assertion messages for array comparisons
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`); // Log a passing message if the arrays are equal
   } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`); // Log a failure message if the arrays are not equal
   }
 };
 
@@ -21,8 +37,14 @@ const eqObjects = function (object1, object2) {
   // Iterate through the keys of object1
   for (const key of keys1) {
     // Check if the values of each key in object1 match the corresponding values in object2
-    if (object1[key] !== object2[key]) {
-      return false; // If any values are different, the objects are not equal
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false; // If arrays don't match, the objects are not equal
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false; // If any values are different, the objects are not equal
+      }
     }
   }
   return true; // If no differences are found, the objects are equal
