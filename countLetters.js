f91dc60dd75ec727ab772d08d180cc09ce9df69f
gisellealmario <gisellealmario@gmail.com>
2023-10-22 16:00:40 +0000

    counting letters

--- countLetters.js
+++ countLetters.js
@@ -1,28 +1,33 @@
 const assertEqual = function(actual, expected) {
-  // Check if the values are equal
   if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`); // Log a passing message if they are equal
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`); // Log a failure message if they are not equal
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-const countLetters = function (arr) {
+const countLetters = function(arr) {
   const lettercount = {};
+  // Loop through each letter in the string array
   for (const letter of arr) {
+    // Skip counting if the letter is a space
+    if (letter === ' ') {
+      continue;
+    }
+    // If the letter exists in the object, increment its count; otherwise, set its count to 1
     if (lettercount[letter]) {
       lettercount[letter]++;
     } else {
       lettercount[letter] = 1;
-
-      }
+    }
   }
   return lettercount;
-
 };
-//test code 
+
+// Test code
 const example = 'LHL';
-
 const result = countLetters(example);
+console.log(result); // Output: { L: 2, H: 1 }
 
-console.log(result);
+assertEqual(result['L'], 2); // Testing the count of 'L'
+assertEqual(result['H'], 1); // Testing the count of 'H'
