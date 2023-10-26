ca9be4a34ee5bd6dc7fbb0f4e9c2ce3ae4d49842
gisellealmario <gisellealmario@gmail.com>
2023-10-26 06:13:14 +0000

    tailtest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,2 +1,17 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual')
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual')
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns 'pepie, aiko, daui' for ['imee, pepie, aiko, daui']", () => {
+    const names = ['imee', 'pepie', 'aiko', 'daui'];
+    const a = tail(names)
+    assert.deepEqual(a, ['pepie', 'aiko', 'daui']); 
+  });
+});
