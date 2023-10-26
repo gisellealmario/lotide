aa9413d13a8977a4c60d0ee22dd5df94b213e948
gisellealmario <gisellealmario@gmail.com>
2023-10-26 01:25:04 +0000

    tailtest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,8 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual')
-module.exports = tail;
 
 //test cases
 
 const name = ['sandra', 'imee', 'peps'];
-
-assertEqual(tail(name), 'sandra')
+console.log(tail(name));
+assertEqual(tail(name), ['sandra', 'imee'])
